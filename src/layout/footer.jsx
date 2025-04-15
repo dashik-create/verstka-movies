f4591e32de32e8c1e5557a7ed5e477b15d6960e5
dashik-create <dasha.tropina.13@mail.ru>
2025-04-15 11:38:57 +0300

    Первый коммит

---
+++ src/layout/footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return <footer className="page-footer sienna accent-1">
+    <div className="footer-copyright">
+      <div className="container">
+        @ Tropina Dasha
+        <a className="grey-text text-lighten-4 right" href="!#">Repository</a>
+      </div>
+    </div>
+  </footer>
+}
+
+export { Footer };
---
+++ src/layout/main.jsx
@@ -0,0 +1,38 @@
+import React,{useState, useEffect} from "react";
+import { Movies, Search, Preloader } from "../components";
+
+
+
+
+export function Main() {
+    const [movies, setMovies] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetchMovies('matrix')
+    }, []) 
+    const fetchMovies = (str, type = "all") => {
+        setLoading(true)
+        fetch(
+            `http://www.omdbapi.com/?apikey=4a8fb7c9&i=tt0133093&s=${str}${
+            type !== "all" ? `&type=${type}` : ""
+            }`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            setMovies(data.Search || [])
+            setLoading(false)
+        })
+    }
+
+    
+        return (
+        <main className="container content">
+            <Search searchMovies={fetchMovies} />
+            {loading ? <Preloader/> : <Movies  movies={movies}/>}
+        </main>
+    );
+    
+        
+}
+
